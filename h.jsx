ff89dd3a0f4ee6cd73357dc28c0fed26194f09c5
rai19astha <120588845+rai19astha@users.noreply.github.com>
2024-06-12 10:52:44 +0530

    Update h.jsx

--- h.jsx
+++ h.jsx
@@ -1,115 +1,145 @@
-import React, { useState, useEffect } from 'react';
-import { Autocomplete, TextField,Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper,Checkbox} from '@mui/material';
-
-import './Barclays.css'; // Import your CSS file here
+import React, { useState } from 'react';
+import {
+    Autocomplete,
+    Checkbox,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+    Paper,
+    TextField,
+} from '@mui/material';
+import Nav from './nav';
+import './Barclays.css';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 
 const Barclays = () => {
     const [inputValue, setInputValue] = useState('');
+    const [selectedOption, setSelectedOption] = useState(null);
     const [showTable, setShowTable] = useState(false);
-    const [btnClicked, setBtnClicked] = useState(false);
 
     const handleInputChange = (event) => {
-      setInputValue(event.target.value);
-      
+        setInputValue(event.target.value);
+        if (selectedOption) {
+            setShowTable(true);
+        } else {
+            setShowTable(false);
+        }
     };
-    
-    const handlebtn1 =()=>{
-        setShowTable(true);
-        setBtnClicked(true);
-    };
-    const myOptions = ['One', 'Two', 'Three', 'Four', 'Five']; 
-    const data = ['One', 'Two', 'Three', 'Four', 'Five']; 
-  return (
-   <>    
-   <nav className="navbar">
-       <div>
-        <h1>THOR</h1>
-      </div>
-      <div className="nav-links">
-        <span><a href="#home">Home</a></span>
-        <span><a href="#about">About</a></span>
-        <span><a href="#contact">Contact</a></span>
-      </div>
-    </nav>
-    <div className="placeholder">
-        <form >
-            <input
-            type="text"
-            value={inputValue}
-            onChange={handleInputChange}
-            placeholder="Request Number"
-            />
-        </form>
-    </div>    
-    <div className='dropdown'>
-        <Autocomplete
-          autoSelect
-          options={myOptions}
-          renderInput={(params) => (
-            <TextField
-              {...params}
-              variant="outlined"
-              label="ESMP"
-              sx={{
-                '& .MuiInputBase-root': {
-                  fontSize: 'medium', // Change the font size of the input
-                  padding: '5px',   // Change the padding of the input
-                  color: '#FFF'
-                },
-                '& .MuiFormLabel-root': {
-                  fontSize: 'medium',  // Change the font size of the label
-                  color: '#FFF',               
-                },
-                '& .MuiSvgIcon-root': {
-                  color: '#FFF' // Change the color of the triangular button
-                }
-              }}
-            />
-          )}
-          componentsProps={{
-            paper: {
-              sx: {
-                backgroundColor: '#151E28', // Change the background of the dropdown menu
-                color: '#FFF', // Change the font color of the dropdown menu
-                maxHeight: '150px', // Set the max height of the dropdown menu
-                overflow: 'auto', // Make the dropdown menu scrollable
-                '&::-webkit-scrollbar': {
-                  display: 'none' // Hide the scrollbar for WebKit browsers
-                },
-                scrollbarWidth: 'none' // Hide the scrollbar for Firefox
-              }
-            }
-          }}
 
-        />
-      </div>
-      <button type="submit" className={`btn1 ${btnClicked ? 'btn1afterclick' : ''}`} onClick={handlebtn1}>GET DATA IN FORM</button>
-      {showTable && data && (
-       <TableContainer className="table-container">
-       <Table>
-           <TableHead>
-               <TableRow>
-                   <TableCell className='tablecell'>Select</TableCell>
-                   <TableCell className='tablecell'>Data</TableCell>
-               </TableRow>
-           </TableHead>
-           <TableBody>
-               {data.map((item, index) => (
-                   <TableRow key={index}>
-                       <TableCell className="select-cell">
-                           <Checkbox />
-                       </TableCell>
-                       <TableCell className="data-cell">{item}</TableCell>
-                   </TableRow>
-               ))}
-           </TableBody>
-       </Table>
-   </TableContainer>
-      )}
-   </>
-  );
-}
+    const handleOptionChange = (event, value) => {
+        setSelectedOption(value);
+        if (inputValue && value) {
+            setShowTable(true);
+        } else {
+            setShowTable(false);
+        }
+    };
+
+    const myOptions = ['One', 'Two', 'Three', 'Four', 'Five'];
+
+    const data = [
+        { sales: '$50,000.00' },
+        { sales: '$10,000.00' },
+        { sales: '$85,000.00' },
+        { sales: '$65,000.00' },
+        { sales: '$98,000.00' },
+        { sales: '$108,000.00' }
+    ];
+
+    return (
+        <>
+            <Nav />
+            <div className="placeholder">
+                <form>
+                    <input
+                        type="text"
+                        value={inputValue}
+                        onChange={handleInputChange}
+                        placeholder="Identifier"
+                        className="input-field"
+                    />
+                </form>
+            </div>
+            <div className="dropdown">
+                <Autocomplete
+                    autoSelect
+                    options={myOptions}
+                    onChange={handleOptionChange}
+                    renderInput={(params) => (
+                        <TextField
+                            {...params}
+                            variant="outlined"
+                            label="ESMP"
+                            sx={{
+                                '& .MuiInputBase-root': {
+                                    fontSize: 'medium',
+                                    padding: '5px',
+                                    color: '#151E28',
+                                },
+                                '& .MuiFormLabel-root': {
+                                    fontSize: 'medium',
+                                    color: '#151E28',
+                                },
+                                '& .MuiSvgIcon-root': {
+                                    color: '#151E28',
+                                },
+                            }}
+                        />
+                    )}
+                    componentsProps={{
+                        paper: {
+                            sx: {
+                                backgroundColor: '#f9f9f9',
+                                color: '#151E28',
+                                maxHeight: '150px',
+                                overflow: 'auto',
+                                '&::-webkit-scrollbar': {
+                                    display: 'none',
+                                },
+                                scrollbarWidth: 'none',
+                            },
+                        },
+                    }}
+                />
+            </div>
+            <div className='tableandbtns'>
+                {showTable && (
+                    <TableContainer component={Paper} className="table-container">
+                        <Table className="table-fill">
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell className="text-left">Select</TableCell>
+                                    <TableCell className="text-left">Sales</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {data.map((item, index) => (
+                                    <TableRow key={index} className={index % 2 === 0 ? 'even-row' : 'odd-row'}>
+                                        <TableCell className="select-cell">
+                                            <Checkbox />
+                                        </TableCell>
+                                        <TableCell className="text-left">{item.sales}</TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                )}
+                <div className='dateandrecent'>
+                    <div className='recentprice'>
+                        <Checkbox />
+                        <span>Recent Prices</span>
+                    </div>
+                    
+                </div>
+
+            </div>
+
+        </>
+    );
+};
 
 export default Barclays;
-
-
