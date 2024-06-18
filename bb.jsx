e9215b9a82f5217a34ec73f04faa6b605ad654f6
rai19astha <120588845+rai19astha@users.noreply.github.com>
2024-06-18 23:40:57 +0530

    Update bb.jsx

--- bb.jsx
+++ bb.jsx
@@ -1,22 +1,7 @@
-
-  const [selectAll, setSelectAll] = useState(false);
-  const [checkedItems, setCheckedItems] = useState({});
-
-  const handleToggleCheckbox = (index) => {
-    const updatedCheckedItems = {
-      ...checkedItems,
-      [index]: !checkedItems[index],
-    };
-    setCheckedItems(updatedCheckedItems);
-    setSelectAll(Object.values(updatedCheckedItems).every(Boolean));
-  };
-
-  const handleSelectAll = (event) => {
-    const { checked } = event.target;
-    const updatedCheckedItems = {};
+  useEffect(() => {
+    const initialCheckedState = {};
     data.forEach((_, index) => {
-      updatedCheckedItems[index] = checked;
+      initialCheckedState[index] = false; // Initialize all checkboxes as unchecked
     });
-    setCheckedItems(updatedCheckedItems);
-    setSelectAll(checked);
-  };
+    setCheckedItems(initialCheckedState);
+  }, [data]);
